1485190411255b49139fab050b4ca13d440350eb
arthur <artyr007@gmail.com>
2024-04-30 22:43:51 +0500

    Add initial file structure
    Structure is defined according to the MVC model: model, view, controller. With an additional module storage. Behaviour and additional modules are subject to change in the future

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { Model as createModel } from "./modules/model";
+import { View as createView } from "./modules/view";
+import { Controller as createController } from "./modules/controller";
+import { Storage as createStorage } from "./modules/storage";
+
+const model = createModel();
+const view = createView();
+const controller = createController();
+const storage = createStorage();
+
+const createWeatherApp = (model, view, controller, storage) => {
+  console.log("%cApp is running", "color: green;");
+  return { model, view, controller, storage };
+};
+
+createWeatherApp(model, view, controller, storage);
