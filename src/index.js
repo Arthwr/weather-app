ae9075a9c52b60918e486914d7566cd12215a2c8
arthur <artyr007@gmail.com>
2024-05-02 22:46:38 +0500

    Set up initial fonts & css for the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Model as createModel } from "./modules/model";
 import { View as createView } from "./modules/view";
 import { Controller as createController } from "./modules/controller";
+import "./styles/app.css"
 
 const model = createModel();
 const view = createView();
