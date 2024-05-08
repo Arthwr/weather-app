a3e63fb0c6c11d75ced0ffa8631aa7770850aa33
arthur <artyr007@gmail.com>
2024-05-08 21:43:47 +0500

    Slightly refactor index.js to improve readability & remove unnecessary functions

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import { View as createView } from "./modules/view";
 import { Controller as createController } from "./modules/controller";
 import "./styles/app.css";
 
-const model = createModel();
-const view = createView();
-const controller = createController(model, view);
+const modelInstance = createModel();
+const viewInstance = createView();
+const controllerInstance = createController(modelInstance, viewInstance);
 
-const createWeatherApp = (model, view, controller) => {
+const createWeatherApp = (controller) => {
   controller.init();
-  return { model, view, controller };
 };
-
-// const weatherApp = 
-createWeatherApp(model, view, controller);
-
-// weatherApp.model.createWeatherData("forecastWeather", "Almaty");
+createWeatherApp(controllerInstance);
