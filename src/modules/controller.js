2c19566a8c01fb08178a6f3a545b1867d5cd2d17
arthur <artyr007@gmail.com>
2024-05-09 17:32:53 +0500

    Add user geolocation detection at init() part of the App to retrieve weather forecast and perform search on it
    Also removed hardcoded mock up data and changed it to placeholders

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,6 +1,6 @@
 export const Controller = (model, view) => {
-  const handleSearch = () => {
-    const query = view.searchInput.value;
+  const handleSearch = (query) => {
+    query = query || view.searchInput.value;
     model
       .createWeatherData("forecastWeather", query)
       .then((weatherData) => {
@@ -21,7 +21,35 @@ export const Controller = (model, view) => {
     }
   };
 
-  const init = () => {
+  const getUserLocation = async () => {
+    if (!navigator.geolocation) {
+      throw new Error("Geolocation is not supported by this browser");
+    }
+
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = `${position.coords.latitude},${position.coords.longitude}`;
+          resolve(userLocation);
+        },
+        (error) => {
+          reject(error.message);
+        }
+      );
+    });
+  };
+
+  const getUserWeatherForecast = async () => {
+    try {
+      const userLocation = await getUserLocation();
+      handleSearch(userLocation);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const init = async () => {
+    await getUserWeatherForecast();
     view.searchForm.addEventListener("submit", (e) => {
       e.preventDefault();
       handleSearch();
